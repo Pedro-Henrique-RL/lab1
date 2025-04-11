b5de8444a09edab94585396c088ea316c5c2a2b4
Pedro Henrique Ribeiro Lima <pedroh130702@gmail.com>
2025-04-10 21:40:37 -0300

    Update banco.py

--- lab1/exercicios em while/banco.py
+++ lab1/exercicios em while/banco.py
@@ -16,20 +16,20 @@ while opcao != 4:
     print("4 - Sair")
     opcao = float(input("Digite uma opção: "))
 
-if opcao == 1:
-    valor = float(input("Valor para sacar: "))
-    if valor <= saldo:
-        saldo = saldo - valor
-        print("Saldo atual", saldo)
+    if opcao == 1:
+        valor = float(input("Valor para sacar: "))
+        if valor <= saldo:
+            saldo = saldo - valor
+            print("Saldo atual", saldo)
+        else:
+            print("Saldo insuficiente!")
+    elif opcao == 2:
+        valor = float(input("Digite o valor a ser depositado: "))
+        if saldo >= 0:
+            saldo = saldo + valor
+    elif opcao == 3:
+        print("Saldo:", saldo)
+    elif opcao == 4:
+        print("Até mais!")
     else:
-        print("Saldo insuficiente!")
-elif opcao == 2:
-    valor = float(input("Digite o valor a ser depositado: "))
-    if saldo >= 0:
-        saldo = saldo + valor
-elif opcao == 3:
-    print("Saldo:", saldo)
-elif opcao == 4:
-    print("Até mais!")
-else:
-    print("Opção invalida!")
+        print("Opção invalida!")
