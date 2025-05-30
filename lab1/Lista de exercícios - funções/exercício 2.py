67d0cd686610b25e6a58bea39fdcfbd8d30667d9
Pedro Henrique Ribeiro Lima <pedroh130702@gmail.com>
2025-05-29 21:28:00 -0300

    Create exercício 2.py

---
+++ lab1/Lista de exercícios - funções/exercício 2.py
@@ -0,0 +1,17 @@
+def eh_primo(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+numero = int(input("Digite um número: "))
+if eh_primo(numero):
+    print(f"{numero} é primo.")
+else:
+    print(f"{numero} não é primo.")
