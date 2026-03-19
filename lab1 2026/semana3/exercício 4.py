07f72de05d0a776eba9c3358cc045022b21371e8
Pedro Henrique Ribeiro Lima <pedroh130702@gmail.com>
2026-03-19 20:06:50 -0300

    Create exercício 4.py

---
+++ lab1 2026/semana3/exercício 4.py
@@ -0,0 +1,4 @@
+gasolina = float(input("Escreva o preço da gasolina: "))
+pagamento = float(input("Digite quanto será pago: "))
+litros = pagamento / gasolina
+print("Você colocou {litros} litros de gasolina")
