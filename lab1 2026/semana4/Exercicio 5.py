d009a6492f378b44522170d4ed5dc9076d5f98ae
Pedro Henrique Ribeiro Lima <pedroh130702@gmail.com>
2026-03-26 22:07:17 -0300

    Create Exercicio 5.py

---
+++ lab1 2026/semana4/Exercicio 5.py
@@ -0,0 +1,10 @@
+alternativa = input("Digite a alternativa escolhida:")
+
+if alternativa == "A":
+    print("Resposta errada")
+elif alternativa == "C":
+    print("Resposta errada")
+elif alternativa == "D":
+    print("Resposta errada")
+else:
+    print("Resposta certa")
