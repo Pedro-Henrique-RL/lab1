5d39c556fd16471f122da67a50dd9a01cd9e767e
Pedro Henrique Ribeiro Lima <pedroh130702@gmail.com>
2026-03-26 21:46:05 -0300

    Create exercicio 2.py

---
+++ lab1 2026/semana4/exercicio 2.py
@@ -0,0 +1,14 @@
+letra = input("Digite uma letra:")
+
+if letra == "A":
+    print("É uma vogal")
+elif letra == "E":
+    print("É uma vogal")    
+elif letra == "I":
+    print("É uma vogal")
+elif letra == "O":
+    print("É uma vogal") 
+elif letra == "U":
+    print("É uma vogal")
+else:
+    print("É uma consoante")
