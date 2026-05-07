c758cd0d0c348a6a564dfca75700495c10c8b76d
Pedro Henrique Ribeiro Lima <pedroh130702@gmail.com>
2026-05-07 19:23:26 -0300

    Create Variáveis.py

---
+++ lab1 2026/Variáveis.py
@@ -0,0 +1,19 @@
+idade = 20                  # int
+nome = "João"               # str
+cpf = "123.456.789-00"      # str
+peso = 72.5                 # float
+dataDeNascimento = "23/12/2004"  # str
+cidade = "São Paulo"        # str
+estado = "SP"               # str
+velocidade = 80.7           # float
+preco = 199.90              # float
+
+print("Idade:", idade)
+print("Nome:", nome)
+print("CPF:", cpf)
+print("Peso:", peso)
+print("Data de Nascimento:", dataDeNascimento)
+print("Cidade:", cidade)
+print("Estado:", estado)
+print("Velocidade:", velocidade)
+print("Preço:", preco)
