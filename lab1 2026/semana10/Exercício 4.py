d12139876be69eb8d23b8e5cf4285b9e6c7efc8e
Pedro Henrique Ribeiro Lima <pedroh130702@gmail.com>
2026-05-14 13:21:58 -0300

    Create Exercício 4.py

---
+++ lab1 2026/semana10/Exercício 4.py
@@ -0,0 +1,21 @@
+
+
+numero = 1
+
+while numero <= 100:
+
+    if numero % 2 == 0:
+        print(numero)
+
+    numero = numero + 1
+
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+while n1 <= n2:
+
+    if n1 % 2 == 0:
+        print(n1)
+
+    n1 = n1 + 1
