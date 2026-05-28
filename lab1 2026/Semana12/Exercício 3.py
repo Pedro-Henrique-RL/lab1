ae458d301d057e0bb6ff7d848a56c768838dc12e
Pedro Henrique Ribeiro Lima <pedroh130702@gmail.com>
2026-05-28 18:45:42 -0300

    Create Exercício 3.py

---
+++ lab1 2026/Semana12/Exercício 3.py
@@ -0,0 +1,11 @@
+
+def dobro(valor):
+    return valor * 2
+
+def triplo(valor):
+    return valor * 3
+
+numero = float(input("Digite um valor: "))
+
+print("Dobro:", dobro(numero))
+print("Triplo:", triplo(numero))
